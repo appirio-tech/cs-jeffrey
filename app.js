6d4d536343bf3e44c4b0189fd96cc9c49eb092b2
Jeff Douglas <jeff@jeffdouglas.com>
2013-06-20 11:03:20 -0400

    fixed typo

--- app.js
+++ app.js
@@ -74,8 +74,8 @@ function updateCmcOrg(challenge) {
           Id: challenge['sobject']['CMC_Task__c'], 
           CloudSpokes_Task_Status__c: challenge['sobject']['Status__c'],
           CloudSpokes_Submission_Count__c: challenge['sobject']['Submissions__c'],
-          CloudSpokes_Challenge_Name__c: challenge['sobject']['Challenge_Name__c'].
-          CloudSpokes_Challenge_Start_Date__c: challenge['sobject']['Challenge_Start_Date__c'].
+          CloudSpokes_Challenge_Name__c: challenge['sobject']['Challenge_Name__c'],
+          CloudSpokes_Challenge_Start_Date__c: challenge['sobject']['Challenge_Start_Date__c'],
           CloudSpokes_Challenge_End_Date__c: challenge['sobject']['Challenge_End_Date__c']
         });
       cmcOrg.update(obj, resp, function(results) {
